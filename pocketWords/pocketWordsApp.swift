20d6f49f003e90dea4df3153e24efc84c4d4810c
Mohammad <ma@bornak.de>
2025-05-12 13:33:09 +0330

    Initial Commit

---
+++ pocketWords/pocketWordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  pocketWordsApp.swift
+//  pocketWords
+//
+//  Created by Mohammad on 5/12/25.
+//
+
+import SwiftUI
+
+@main
+struct pocketWordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ pocketWordsUITests/pocketWordsUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  pocketWordsUITestsLaunchTests.swift
+//  pocketWordsUITests
+//
+//  Created by Mohammad on 5/12/25.
+//
+
+import XCTest
+
+final class pocketWordsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
