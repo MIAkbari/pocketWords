b2ba87960294a2fa797bd6075ce9b366e4e449a5
Mohammad <makbariengineer@gmail.com>
2025-05-13 12:38:43 +0330

    # Updated View also add mark comment

--- pocketWords/Views/AddCardView.swift
+++ pocketWords/Views/AddCardView.swift
@@ -11,38 +11,61 @@ import SwiftUI
 import SwiftData
 
 struct AddCardView: View {
+    
+    // MARK: - Properties
+    
     @Environment(\.dismiss) private var dismiss
     @Environment(\.modelContext) private var modelContext
-
+    
     @State private var word = ""
     @State private var meaning = ""
-
+    
+    // MARK: - Body . Init
+    
     var body: some View {
+        mainView
+    }
+    
+    // MARK: - Views
+    
+    private var mainView: some View {
         NavigationStack {
-            Form {
-                TextField("Word", text: $word)
-                TextField("Meaning", text: $meaning)
-            }
-            .navigationTitle("New Word")
-            .toolbar {
-                ToolbarItem(placement: .confirmationAction) {
-                    Button("Save") {
-                        let newCard = WordCard(word: word, meaning: meaning)
-                        modelContext.insert(newCard)
-                        dismiss()
-                    }
-                    .disabled(word.isEmpty || meaning.isEmpty)
-                }
-                ToolbarItem(placement: .cancellationAction) {
-                    Button("Cancel") {
-                        dismiss()
+            formView
+                .navigationTitle("New Word")
+                .toolbar {
+                    ToolbarItem(placement: .confirmationAction) {
+                        saveButtonView
                     }
                 }
-            }
         }
+    }
+    
+    private var formView: some View {
+        Form {
+            TextField("Word", text: $word)
+                .accessibilityLabel("TextField Save Word")
+                .accessibilityHint("Input to save the new word card")
+            
+            TextField("Meaning", text: $meaning)
+                .accessibilityLabel("TextField Save Meaning")
+                .accessibilityHint("Input to save the new Meaning card")
+        }
+    }
+    
+    private var saveButtonView: some View {
+        Button("Save") {
+            let newCard = WordCard(word: word, meaning: meaning)
+            modelContext.insert(newCard)
+            dismiss()
+        }
+        .disabled(word.isEmpty || meaning.isEmpty)
+        .accessibilityLabel("Save Word")
+        .accessibilityHint("Tap to save the new word card")
+
     }
 }
 
+// MARK: - Preview
 
 #Preview {
     AddCardView()
--- pocketWords/pocketWordsApp.swift
+++ pocketWords/pocketWordsApp.swift
@@ -11,7 +11,10 @@ import SwiftUI
 struct pocketWordsApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationStack {
+                FlashcardView()
+            }
+            .modelContainer(for: WordCard.self)
         }
     }
 }
